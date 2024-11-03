7043b3105df851e226cc5e518bccdfec49edff36
letfonsecaa <leticia.santos@sptech.school>
2024-11-03 13:37:26 -0300

    ajustes no DER e scipt do BD

--- src/database/innovaxDB.sql
+++ src/database/innovaxDB.sql
@@ -3,72 +3,72 @@ create database if not exists InnovaxDB;
 use InnovaxDB;
 
 create table empresa (
-idEmpresa int not null auto_increment,
-nome varchar(50) not null,
-email varchar(45) not null,
+id int not null auto_increment,
+razaoSocial varchar(80) not null,
+nomeFantasia varchar(50) not null,
 cnpj varchar(14) default null,
-senha varchar(45) not null,
-primary key (idEmpresa),
-unique key email (email),
+primary key (id),
 unique key cnpj (cnpj)
 );
 
-create table funcionario (
-idFuncionario int not null auto_increment,
+create table usuario (
+id int not null auto_increment,
 nome varchar(50) not null,
 cpf varchar(11) default null,
 email varchar(45) not null,
 senha varchar(45) not null,
-primary key (idFuncionario),
+hierarquia varchar(45),
+primary key (id),
 unique key email (email),
-unique key cnpj (cpf),
-fkEmpresaFuncio int,
-constraint fk_empresa_funcio foreign key (fkEmpresaFuncio) references empresa(idEmpresa)
+unique key cpf (cpf),
+fkEmpresa int,
+constraint fk_empresa_funcio foreign key (fkEmpresa) references empresa(id)
 )auto_increment=10;
 
-create table dados (
-idDados int primary key not null auto_increment,
+create table leitura (
+id int primary key not null auto_increment,
 areaDesmatada decimal (4,2) not null,
 temperaturaMensal  decimal (4,2) not null,
 precipitacaoMensal decimal (4,2) not null,
 cidade varchar(45),
-unidadeFederativa varchar(50),
+unidadeFederativa varchar(50) not null,
 mes tinyint, 
 ano year
 )auto_increment=100;
 
+create table promptIA (
+id int primary key auto_increment,
+descricao varchar(1000)
+);
 
-create table recomendacoesIA (
-idRecomendacoes int primary key not null auto_increment,
-unidadeFederativa varchar(50) not null,
+create table recomendacaoIA (
+id int primary key not null auto_increment,
 recomendacao varchar(100) not null,
+unidadeFederativa varchar(50) not null,
+dataHora datetime not null,
 fkEmpresa int,
-constraint fk_empresa foreign key (fkEmpresa) references empresa(idEmpresa)
+fkPrompt int,
+constraint fk_empresa foreign key (fkEmpresa) references empresa(id),
+constraint fk_prompt foreign key (fkPrompt) references promptIA(id)
 )auto_increment=1000;
 
 
-create table parametrosRecomendacoes (
-idParametros int primary key not null auto_increment,
-limiteArea decimal (4,2),
-horarioNotificacoes time,
-fkEmpresaRecomendacoes int,
-constraint fk_empresa_recom foreign key (fkEmpresaRecomendacoes) references empresa(idEmpresa)
+create table parametroRecomendacao (
+id int primary key not null auto_increment,
+parametro json,
+fkEmpresa int,
+constraint fk_empresa_recom foreign key (fkEmpresa) references empresa(id)
 );
 
 
-create table logsJAR (
-idLog int primary key auto_increment,
+create table logJAR (
+id int primary key auto_increment,
 descricao varchar(1000),
 dataHora datetime,
-fkLogEmpresa int,
-constraint fk_empresa_log foreign key (fkLogEmpresa) references empresa(idEmpresa)
+fkEmpresa int,
+constraint fk_empresa_log foreign key (fkEmpresa) references empresa(id)
 );
 
-create table promptIA (
-idPrompt int primary key auto_increment,
-descricao varchar(1000),
-dataHora datetime
-);
 
 
 
