225619a843e4d119ab06cedcb96402abfea3ac20
migue <miguel.santos6700@gmail.com>
2024-11-22 19:17:52 -0300

    Feito rota de leitura e feito consulta no banco com subconsulta

--- src/models/leituraModel.js
+++ src/models/leituraModel.js
@@ -1,7 +1,21 @@
 var database = require("../database/config")
 
 function getLeituraByIdEmpresa(idEmpresa) {
-    const sql = `SELECT * FROM leitura WHERE fkEmpresa = ${idEmpresa};`
+    const sql = `
+        SELECT *
+        FROM leitura l
+        WHERE l.ano = (
+            SELECT MAX(l2.ano)
+            FROM leitura l2
+            WHERE l2.unidadeFederativa = l.unidadeFederativa
+        ) 
+        AND l.mes = (
+            SELECT MAX(l3.mes)
+            FROM leitura l3
+            WHERE l3.unidadeFederativa = l.unidadeFederativa
+            AND l3.ano = l.ano
+        )
+        AND fkEmpresa = 8;`
     return database.executar(sql)
 }
 
